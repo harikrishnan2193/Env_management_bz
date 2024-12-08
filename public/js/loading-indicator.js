969891e39b9fccdb29660244455b265663a530d2
abilash <abilashb2000@gmail.com>
2024-12-08 20:57:38 +0530

    fixed loading errors and deleted duplicate code in side.ejs, projectEnve.ejs

--- public/js/loading-indicator.js
+++ public/js/loading-indicator.js
@@ -2,14 +2,19 @@ const pageLoadingIndicator = document.getElementById("pageLoadingIndicator");
 
 // Event delegation for all links
 document.body.addEventListener("click", (event) => {
+  console.log("event:", event);
   const target = event.target.closest("a");
+  console.log("target", target);
   if (target && target.href && !target.href.startsWith("#")) {
     pageLoadingIndicator.classList.remove("hidden");
   }
+
+  console.log(pageLoadingIndicator.classList);
 });
 
 // Event delegation for form submissions
 document.body.addEventListener("submit", (event) => {
+  console.log(event);
   const form = event.target.closest("form");
   if (form) {
     pageLoadingIndicator.classList.remove("hidden");
@@ -18,5 +23,8 @@ document.body.addEventListener("submit", (event) => {
 
 // Hide spinner when the page is shown again (cached navigation)
 window.addEventListener("pageshow", () => {
+  console.log("pageshow");
+
+  console.log(pageLoadingIndicator.classList);
   pageLoadingIndicator.classList.add("hidden");
 });
